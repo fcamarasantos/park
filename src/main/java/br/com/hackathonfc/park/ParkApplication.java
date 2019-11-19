d836d59118b69ad4354f13088d6fc5f9d367d431
Rodrigo Rodrigues Carvalho <rodrigo.carvalho@fcamara.com.br>
2019-11-19 18:23:14 -0300

    versao inicial

---
+++ src/main/java/br/com/hackathonfc/park/ParkApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackathonfc.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hackathonfc/park/ParkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hackathonfc.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
