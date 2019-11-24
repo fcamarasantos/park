29a48a9c0f52c34fa7af12110b8869bccd81e0f3
Lucas Penha <lspenhacontato@gmail.com>
2019-11-24 11:23:33 -0300

    ATT Front

---
+++ front/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  $('.cpf').mask('000.000.000-00');
+  $('.cep').mask('00000-000');
+  $('.card').mask('0000.0000.0000.0000');
+  $('#inputCodigo').mask('000');
+  $('[data-toggle="tooltip"]').tooltip()
+});
