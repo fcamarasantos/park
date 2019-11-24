47784442d1500b57f8d05ea6737bac3bc64c02b1
hicolazu <hicolazu22@hotmail.com>
2019-11-24 09:42:50 -0300

    add vagaModel

---
+++ src/main/java/br/com/hackathonfc/park/model/Vaga.java
@@ -0,0 +1,73 @@
+package br.com.hackathonfc.park.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Vaga {
+
+		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@ManyToOne
+		private Estacionamento estacionamento;
+		
+		@OneToOne
+		private Carro carro;
+		
+		private LocalDateTime dataInicio;
+		
+		private LocalDateTime dataSaida;
+		
+		private boolean livre;
+		
+		public Vaga() {
+			
+		}
+
+		public Vaga(Long id, Estacionamento estacionamento, Carro carro, LocalDateTime dataInicio,
+				LocalDateTime dataSaida, boolean livre) {
+			this.id = id;
+			this.estacionamento = estacionamento;
+			this.carro = carro;
+			this.dataInicio = dataInicio;
+			this.dataSaida = dataSaida;
+			this.livre = livre;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public Estacionamento getEstacionamento() {
+			return estacionamento;
+		}
+
+		public Carro getCarro() {
+			return carro;
+		}
+
+		public LocalDateTime getDataInicio() {
+			return dataInicio;
+		}
+
+		public LocalDateTime getDataSaida() {
+			return dataSaida;
+		}
+
+		public boolean isLivre() {
+			return livre;
+		}
+		
+		
+		
+		
+		
+		
+}
