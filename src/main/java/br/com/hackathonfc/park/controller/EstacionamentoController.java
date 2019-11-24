ff26dbbf43462da3e164a23e402680189c30e40f
hicolazu <hicolazu22@hotmail.com>
2019-11-24 10:02:41 -0300

    add repository

--- src/main/java/br/com/hackathonfc/park/controller/EstacionamentoController.java
+++ src/main/java/br/com/hackathonfc/park/controller/EstacionamentoController.java
@@ -1,8 +1,34 @@
 package br.com.hackathonfc.park.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.hackathonfc.park.repository.*;
+import br.com.alura.forum.controllers.dto.TopicoDto;
+import br.com.alura.forum.model.Topico;
+import br.com.hackathonfc.park.model.Estacionamento;
 
 @RestController
 public class EstacionamentoController {
-
+	
+	@Autowired
+	private EstacionamentoRepository estacionamentoRepository;
+	
+	@GetMapping("/")
+	public Page<Estacionamento> listar(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
+		
+		Page<Estacionamento> estacionamentos;
+		
+		estacionamentos = estacionamentoRepository.findAll(paginacao);
+		
+		return ;
+	}
 }
---
+++ src/main/java/br/com/hackathonfc/park/repository/EstacionamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.hackathonfc.park.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstacionamentoRepository extends JpaRepository{
+	
+}
