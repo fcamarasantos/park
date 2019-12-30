a6964214278961dbe0192abdf6398b1be968f9ce
Henrico <hicolazu22@hotmail.com>
2019-12-30 10:39:54 -0300

    Fix Delete Estacionamento

--- src/main/java/br/com/hackathonfc/park/model/Estacionamento.java
+++ src/main/java/br/com/hackathonfc/park/model/Estacionamento.java
@@ -1,6 +1,9 @@
 package br.com.hackathonfc.park.model;
 
 import br.com.hackathonfc.park.dto.EstacionamentoDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +11,9 @@ import java.util.List;
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Estacionamento {
 	
 	@Id
@@ -30,20 +36,6 @@ public class Estacionamento {
 	
 	@OneToMany(mappedBy = "estacionamento", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Vaga> vagas = new ArrayList<>();
-	
-	public Estacionamento() {
-		
-	}
-	
-	public Estacionamento(String nome, int cnpj, String endereco, int telefone, int vagasMotos, int vagasCarros, Double precoHora) {
-		this.nome = nome;
-		this.cnpj = cnpj;
-		this.endereco = endereco;
-		this.telefone = telefone;
-		this.vagasMotos = vagasMotos;
-		this.vagasCarros = vagasCarros;
-		this.precoHora = precoHora;
-	}
 
     public Estacionamento(EstacionamentoDTO estacionamentoDTO) {
 		this.nome = estacionamentoDTO.getNome();
@@ -54,83 +46,4 @@ public class Estacionamento {
 		this.vagasMotos = estacionamentoDTO.getVagasMotos();
 		this.precoHora = estacionamentoDTO.getPrecoHora();
     }
-
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Estacionamento other = (Estacionamento) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getCnpj() {
-		return cnpj;
-	}
-	public void setCnpj(int cnpj) {
-		this.cnpj = cnpj;
-	}
-	public int getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(int telefone) {
-		this.telefone = telefone;
-	}
-	public int getVagasMotos() {
-		return vagasMotos;
-	}
-	public void setVagasMotos(int vagasMotos) {
-		this.vagasMotos = vagasMotos;
-	}
-	public int getVagasCarros() {
-		return vagasCarros;
-	}
-	public void setVagasCarros(int vagasCarros) {
-		this.vagasCarros = vagasCarros;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public Double getPrecoHora() {
-		return precoHora;
-	}
-
-	public void setPrecoHora(Double precoHora) {
-		this.precoHora = precoHora;
-	}
-
 }
--- src/main/java/br/com/hackathonfc/park/model/Vaga.java
+++ src/main/java/br/com/hackathonfc/park/model/Vaga.java
@@ -1,18 +1,24 @@
 package br.com.hackathonfc.park.model;
 
 import br.com.hackathonfc.park.dto.VagaDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vaga {
 
 		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
-		
-		@ManyToOne(cascade = CascadeType.ALL)
+
+		@ManyToOne
 		private Estacionamento estacionamento;
 		
 		private LocalDateTime dataInicio;
@@ -21,69 +27,20 @@ public class Vaga {
 		
 		private boolean livre;
 
-		@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+		@OneToOne(mappedBy = "vaga", cascade = CascadeType.ALL, orphanRemoval = true)
 		private Veiculo veiculo;
-		
-		public Vaga() {
-		}
 
-		public Vaga(Estacionamento estacionamento, LocalDateTime dataInicio, LocalDateTime dataSaida, boolean livre) {
+	public Vaga(Estacionamento estacionamento, LocalDateTime dataInicio, LocalDateTime dataSaida, boolean livre) {
 			this.estacionamento = estacionamento;
 			this.dataInicio = dataInicio;
 			this.dataSaida = dataSaida;
 			this.livre = livre;
 		}
 
-    public Vaga(VagaDTO vagaDTO) {
+    	public Vaga(VagaDTO vagaDTO) {
 			this.estacionamento = vagaDTO.getEstacionamento();
 			this.dataInicio = vagaDTO.getDataInicio();
 			this.dataSaida = vagaDTO.getDataSaida();
 			this.livre = vagaDTO.isLivre();
-    }
-
-    @Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Vaga other = (Vaga) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			return true;
-		}
-
-		public Long getId() {
-			return id;
-		}
-
-		public Estacionamento getEstacionamento() {
-			return estacionamento;
-		}
-
-		public LocalDateTime getDataInicio() {
-			return dataInicio;
-		}
-
-		public LocalDateTime getDataSaida() {
-			return dataSaida;
-		}
-
-		public boolean isLivre() {
-			return livre;
-		}
-		
+    	}
 }
